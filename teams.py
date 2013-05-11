9a1d7b076f41d00a388c4538796d75bd97f31a06
thomasgautvedt <thomasgautv@hotmail.com>
2013-05-11 23:35:58 +0200

    WIP generating of teams, renamed the game-file

--- teams.py
+++ teams.py
@@ -11,6 +11,7 @@ import os
 # All the existing names
 names = open("names.txt", "r", encoding = "utf8")
 names_lines = names.readlines()
+names.close()
 
 # The main-method
 def main():
@@ -43,14 +44,24 @@ def main():
             if ((response == 'y' and (len(names_lines) % 2 == 0)) or response == 'a' or response == 'n'):
                 # Valid response
                 if (response == 'y'):
-                    # Using the old list
+                    # Using the old list. Calling the method
                     generate_team()
                 elif (response == 'a'):
-                    # Appending new names
-                    varrrr = 1
+                    # Appending new names to the list. Printing instructions
+                    print('')
+                    print('Legg til nye navn i lista. Begynn spillet ved å skrive [p].')
+                    print('')
+                    
+                    # Calling method
+                    append_names()
                 else:
-                    # Totally new list
-                    varrrr = 1
+                    # Making a new list. Printing instructions
+                    print('')
+                    print('Legg til nye navn i lista. Begynn spillet ved å skrive [p].')
+                    print('')
+                    
+                    # Calling the method
+                    new_names()
                 break
             else:
                 # Not a valid response, provide feedback and continue the loop
@@ -60,32 +71,99 @@ def main():
 
 # Method to generate the teams based on the names we have in the file right now
 def generate_team():
+    # Printing instructions
+    print('')
+    print('Lagene er som følger:')
+    print('')
+    
+    # Some lines
     i = 0
     lagNr = 1
     linje = ""
-    while len(lag) > 0:
-        if len(lag) == 1:
+    
+    # Cleaning the file with existing teams
+    # First removing all the content in the file
+    open('teams.txt', 'w').close()
+    
+    # Opening the file with all the names
+    names = open("names.txt", "r", encoding = "utf8")
+    names_lines = names.readlines()
+    names.close()
+    
+    # Looping all the names
+    while len(names_lines) > 0:
+        if len(names_lines) == 1:
             rand = 0
         else:
-            rand = random.randrange(0,len(lag)-1)
+            rand = random.randrange(0,len(names_lines)-1)
         
         if i % 2 != 0:
-            print("Lag "+str(lagNr)+" er: "+linje+" og "+lag[rand])
+            print("Lag "+str(lagNr)+": "+linje+" og "+names_lines[rand].strip())
             lagNr += 1
         else:
-            linje = lag[rand]
-            lag.pop(rand)
+            linje = names_lines[rand].strip()
+        
+        names_lines.pop(rand)
         i += 1
+    
+    # Let's play!
+    os.system('python3 game1.py')
 
 # Method to append names to the file
 def append_names():
-    # TODO
-    varrrr = 1
+    # Looping 'till done
+    while True:
+        # Getting response
+        name = input('Navn: ')
+        
+        # Checking if adding or quitting
+        if (name == 'p'):
+            # Trying to quit. Do we have an even number of teams?
+            names = open("names.txt", "r", encoding = "utf8")
+            names_lines = names.readlines()
+            names.close()
+            
+            # Checking even/odd
+            if (len(names_lines) % 2 == 0):
+                # Even number, lets generate some teams!
+                generate_team()
+                break
+            else:
+                print('')
+                print('Beklager, lagene må gå opp i to. Dere har '+str(len(names_lines))+' navn. Legg til en til!')
+                print('')
+        else:
+            # Adding name, check to see if duplicate
+            names = open("names.txt", "r", encoding = "utf8")
+            names_lines = names.readlines()
+            names.close()
+            
+            names_contains_name = False
+            
+            # Checking for duplicates in the list
+            for i in range(len(names_lines)):
+                if (names_lines[i].strip().lower() == name.lower()):
+                    # Duplicate
+                    names_contains_name = True
+                    break
+            
+            # Checking result from loop above
+            if (names_contains_name):
+                # We have ourselvs a duplicate!
+                print('Navnet finnes i listen fra før!')
+            else:
+                # We have no duplicates in here! Append name to file
+                with open("names.txt", "a") as myfile:
+                    myfile.write(name+'\n')
+                    myfile.close()
 
 # Method to add new names to the file
 def new_names():
-    # TODO
-    varrrr = 1
+    # First removing all the content in the file
+    open('names.txt', 'w').close()
+    
+    # Call method to add new names
+    append_names()
 
 
 # Calling the main-method!
